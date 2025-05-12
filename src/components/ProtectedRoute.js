508bd447ece58d38a15c7da9f3a67f768d61963d
Dahiru Aliyu Adamu <dahirualiyuadamu@gmail.com>
2025-05-12 07:41:40 +0100

    Updating Loader to Solar System

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,16 +1,18 @@
 import React, { useContext } from 'react'
 import { Navigate, Outlet } from 'react-router-dom'
 import { AuthContext } from '../helpers/AuthContext'
+import SolarSystemLoader from './SolarSystemLoader'
 
 // const ProtectedRoute = ({ children }) => {
 const ProtectedRoute = () => {
     const { authState, loading } = useContext(AuthContext)
 
-    if (loading) return (
-        <div className="min-h-screen flex items-center justify-center mt-0">
-            <div className="w-10 h-10 border-4 border-blue-500 border-t-transparent rounded-full animate-spin" />
-        </div>
-    )
+    if (loading) return <SolarSystemLoader />
+    // if (loading) return (
+    //     <div className="min-h-screen flex items-center justify-center mt-0">
+    //         <div className="w-10 h-10 border-4 border-blue-500 border-t-transparent rounded-full animate-spin" />
+    //     </div>
+    // )
     
     // return authState.status ? children : <Navigate to='/login' replace />
     return authState.status ? <Outlet /> : <Navigate to='/login' replace />
